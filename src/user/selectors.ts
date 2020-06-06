91a483cad13ee5fc8a75a236d12ee7e044f8d7d3
Su Anli <sual0001@gmail.com>
2020-06-06 17:20:13 +0800

    feat(analytics): add user id and log event for dark mode

--- src/user/selectors.ts
+++ src/user/selectors.ts
@@ -2,4 +2,6 @@ import {State} from './type';
 
 export default class UserSelectors {
   static isDarkMode = (state: State) => state.isDarkMode;
+  /* istanbul ignore next */
+  static userId = (state: State) => state.userId;
 }
